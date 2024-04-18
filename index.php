d4ed1099b66d1780774ccd26fa7de96ce62a1260
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-04-18 16:53:22 +0200

    add setter and getter functions in class Production

--- index.php
+++ index.php
@@ -2,20 +2,35 @@
 
 class Production{
 
+    public $year;
+
     public function __construct(public string $title, public string $lang,public int $vote) {       
+    }
+    public function setNewVote($newVote) {
+        $this->vote = $newVote;
+    }
+    public function setYear($year) {
+        $this->year = $year;
+    }
+    public function getYear() {
+        return "The film '$this->title' was released in $this->year";
+    }
 }
 
-}
 $parasite = new Production('Parasite','Korean',8);
 $gladiator = new Production('The Gladiator','English (USA)',10);
 $itaJeegRobot = new Production('Lo chiamavano Jeeg Robot','Italian',8);
 
+$parasite->setYear(2019);
+$gladiator->setYear(2000);
+$itaJeegRobot->setYear(2015);
 
 $films=[
     $parasite,
     $gladiator,
     $itaJeegRobot
 ];
+
 
 //var_dump($films);
 ?>
@@ -33,8 +48,13 @@ $films=[
             <li>Title: <strong><?=$film->title?></strong></li>
             <li>Language: <?=$film->lang?></li>
             <li>Vote: <?=$film->vote?></li>
+<!--             <li>Year:  //$film->year</li>
+ -->            <li>
+                <small>Note: <?=$film->getYear()?></small>
+            </li>
             <hr>
         <?php endforeach?>
     </ul>
+
 </body>
 </html>
